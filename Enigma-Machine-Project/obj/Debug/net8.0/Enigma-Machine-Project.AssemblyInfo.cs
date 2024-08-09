9a509c9262d57bc0bc7d9de737bfeb190719af42
Moni2352 <marcus.labrecque@aspengroveschool.com>
2024-08-08 18:43:10 -0700

    publish update

--- Enigma-Machine-Project/obj/Debug/net8.0/Enigma-Machine-Project.AssemblyInfo.cs
+++ Enigma-Machine-Project/obj/Debug/net8.0/Enigma-Machine-Project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Enigma-Machine-Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1fb19273f295c75a4610b8fa3fd2aef4d6dbfda3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2906c926c7c05be5f1e0ec6c697fe1cac56e9fa3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Enigma-Machine-Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Enigma-Machine-Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
