fc3580f8ce98f124375c395b745ec1bad2978eb2
Moni2352 <marcus.labrecque@aspengroveschool.com>
2024-08-16 17:19:58 -0700

    Added stuff

--- Enigma-Machine-Project/Renderer.cs
+++ Enigma-Machine-Project/Renderer.cs
@@ -16,107 +16,19 @@ namespace IMGUI_Render
     {
         Program Programs = new Program();
 
-        string[] discs = { "Rotor 1", "Rotor 2", "Rotor 3", "Rotor 4", "Rotor 5", "Rotor 6", "Rotor 7", "Rotor 8", "Rotor 9", "Rotor 10", "Rotor 11", "Rotor 12", "Rotor 13", "Rotor 14", "Rotor 15", "Rotor 16", "Rotor 17", "Rotor 18", "Rotor 19", "Rotor 20" };
-        public static int[] selectedIndices = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]; // Indices for the selected elements in each dropdown
+        string[] A = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", ".", ",", "?", "!"};
 
-        public static int NumOfRotors = 3;
+        string[] B = { };
 
-        public static int[] RotorRotations = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-        public static string[] RotorNames = { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J" };
-
-        string textinput = string.Empty;
-        string ConvertedText = "";
         protected override void Render()
         {
-            // Draw stuff here
-            ImGui.Begin("Enigma-Machine | Version 0.1");
 
-            ImGui.BeginTabBar("Tabs");
-            if (ImGui.BeginTabItem("Conversion"))
+            ImGui.Begin("Enigma-Machine-Disc-Writer");
+
+            for (int i = 0; i < A.Length; i++)
             {
-                ImGui.InputText("Text to Convert", ref textinput, 500);
-                if (ImGui.Button("Convert"))
-                {
-                    ConvertedText = Programs.Convert(textinput);
-                }
-
-                ImGui.Text($"Converted Text: {ConvertedText}");
-
-                if(ImGui.Button("Exit"))
-                {
-                    Environment.Exit(0);
-                }
-
-                ImGui.EndTabItem();
+                ImGui.InputText($"Coded Symbol For: {A[i]}", ref B[i], 1);
             }
-
-            if (ImGui.BeginTabItem("Settings"))
-            {
-
-                ImGui.SliderInt("Number Of Rotors", ref NumOfRotors, 3, 10);
-
-                for (int comboIndex = 0; comboIndex < NumOfRotors; comboIndex++)
-                {
-                    // Display the current selection or "Select..." if none is selected
-                    string preview = selectedIndices[comboIndex] >= 0 ? discs[selectedIndices[comboIndex]] : "Select...";
-                    if (ImGui.BeginCombo($"Slot {comboIndex + 1}", preview))
-                    {
-                        for (int i = 0; i < discs.Length; i++)
-                        {
-                            // Check if the current disc is selected by any other dropdown
-                            bool isTaken = false;
-                            for (int j = 0; j < selectedIndices.Length; j++)
-                            {
-                                if (j != comboIndex && selectedIndices[j] == i)
-                                {
-                                    isTaken = true;
-                                    break;
-                                }
-                            }
-
-                            // Only show selectable items that are not taken
-                            if (!isTaken)
-                            {
-                                bool isSelected = (selectedIndices[comboIndex] == i);
-                                if (ImGui.Selectable(discs[i], isSelected))
-                                {
-                                    selectedIndices[comboIndex] = i; // Update selected index
-                                }
-
-                                if (isSelected)
-                                {
-                                    ImGui.SetItemDefaultFocus();
-                                }
-                            }
-                        }
-                        ImGui.EndCombo();
-                    }
-                }
-                // Drop Down Menus
-
-            
-                for (int RotationIndex = 0; RotationIndex < NumOfRotors; RotationIndex++)
-                {
-                    ImGui.SliderInt($"Rotation Of Rotor {RotorNames[RotationIndex]}", ref RotorRotations[RotationIndex], 0, 40);
-                }
-
-                if (ImGui.Button("Reset Settings"))
-                {
-                    NumOfRotors = 3;
-                    selectedIndices = [0,1,2, 3, 4, 5, 6, 7, 8, 9];
-
-                    for (int i = 0; i < 10; i++)
-                    {
-                        RotorRotations[i] = 0;
-                    }
-                }
-
-                ImGui.EndTabItem();
-            }
-            
-            ImGui.EndTabBar();
-
-            ImGui.End();
 
         }
     }
--- Enigma-Machine-Project/obj/Debug/net8.0/Enigma-Machine-Project.AssemblyInfo.cs
+++ Enigma-Machine-Project/obj/Debug/net8.0/Enigma-Machine-Project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Enigma-Machine-Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+be35234c3a8d5716f93188ffd5c232881ea565d8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+948dfddf9f67bfa78512ee43b55861b4c557c835")]
 [assembly: System.Reflection.AssemblyProductAttribute("Enigma-Machine-Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Enigma-Machine-Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
