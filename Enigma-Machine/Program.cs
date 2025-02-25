7a4287fd89b6d796d91f9a379c8485e829cc0e28
Moni2352 <marcus.labrecque@aspengroveschool.com>
2025-02-25 09:43:43 -0800

    Add Some Comments

--- Enigma-Machine/Program.cs
+++ Enigma-Machine/Program.cs
@@ -2,3 +2,7 @@
 
 Console.WriteLine("Please Enter Text To Decode");
 Console.ReadKey();
+
+// Get Text To Decode
+// Ask If Any Word Is Known to be in The Text
+// Ask If Any Of The Rotors Are Known
